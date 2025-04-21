363311d0d90818c1891eb4fa131d1292a5ae4cee
Paul Laskowski <paul@warmbowski.org>
2025-04-20 21:42:04 -0700

    initial setup

---
+++ src/routes/__root.tsx
@@ -0,0 +1,71 @@
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRouteWithContext,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+
+import Header from '../components/Header'
+
+import ConvexProvider from '../integrations/convex/provider'
+
+import TanstackQueryLayout from '../integrations/tanstack-query/layout'
+
+import appCss from '../styles.css?url'
+
+import type { QueryClient } from '@tanstack/react-query'
+
+interface MyRouterContext {
+  queryClient: QueryClient
+}
+
+export const Route = createRootRouteWithContext<MyRouterContext>()({
+  head: () => ({
+    meta: [
+      {
+        charSet: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+    links: [
+      {
+        rel: 'stylesheet',
+        href: appCss,
+      },
+    ],
+  }),
+
+  component: () => (
+    <RootDocument>
+      <ConvexProvider>
+        <Header />
+
+        <Outlet />
+        <TanStackRouterDevtools />
+
+        <TanstackQueryLayout />
+      </ConvexProvider>
+    </RootDocument>
+  ),
+})
+
+function RootDocument({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head>
+        <HeadContent />
+      </head>
+      <body>
+        {children}
+        <Scripts />
+      </body>
+    </html>
+  )
+}
